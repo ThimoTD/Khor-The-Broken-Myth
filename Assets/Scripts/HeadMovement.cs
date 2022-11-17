ce480494b8e0cd931131747a08b2a321e805315a
damnnn <damianojongerius@gmail.com>
2022-11-17 20:34:06 +0100

    rb.velo to rb.addforce

--- Assets/Scripts/HeadMovement.cs
+++ Assets/Scripts/HeadMovement.cs
@@ -4,23 +4,20 @@ using UnityEngine;
 
 public class HeadMovement : MonoBehaviour
 {
-    public float horizontal;
+    private float horizontal;
+    private float inputSpeed;
+
     public float speed;
     public float jumpingPower;
     public bool facingRight;
+
+    public float maxSpeed;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
+    //per tick
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
@@ -29,13 +26,43 @@ public class HeadMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
+        
+ 
 
-        transform.rotation 
+        if(horizontal == 0)
+        {
+            if (rb.angularVelocity != 0)
+            {
+                rb.angularVelocity = Mathf.Lerp(rb.angularVelocity, 0, 0.003f);
+
+            }
+            inputSpeed = 0;
+        }
+
     }
 
+    //per frame
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        //thimo ik heb velocity veranderd naar addforce zodat het draaien er beter uitziet
+        //zorg ervoor dat je sneller kan stoppen en starten want de snelheid is best fucked up
+        inputSpeed = Mathf.Lerp(inputSpeed, horizontal * speed, 0.03f);
+        //rb.velocity = new Vector2((inputSpeed * 250) * Time.deltaTime, rb.velocity.y);
+        rb.AddForce(new Vector2((inputSpeed * 250) * Time.deltaTime, rb.velocity.y), ForceMode2D.Force);
+        //if not rolling in idle 
+        if(horizontal != 0)
+        {
+            MaxSpeed();
+        }
+
+    }
+
+    private void MaxSpeed()
+    {
+        if(rb.velocity.x > maxSpeed)
+        {
+            rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+        }
     }
 
     private bool IsGrounded()
